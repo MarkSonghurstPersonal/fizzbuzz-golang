c53d8cad86070c6a3b9e6067e5712569253541f5
Mark Songhurst <MarkSonghurst@users.noreply.github.com>
2025-04-12 18:50:55 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "log"
+
+func main() {
+
+	for i := range 100 {
+
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			log.Println("FizzBuzz")
+
+		case i%3 == 0:
+			log.Println("Fizz")
+
+		case i%5 == 0:
+			log.Println("Buzz")
+
+		default:
+			log.Println(i)
+		}
+	}
+}
